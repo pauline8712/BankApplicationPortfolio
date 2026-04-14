092ceebc7c78e0d2dcb66db09cdff1c725be47b1
Pauline <plinekaeorg@gmail.com>
2026-04-14 20:11:34 +0200

    Finished with AppDbContext but there are errors

--- BudgetApp.Infrastructure/Data/AppDbContext.cs
+++ BudgetApp.Infrastructure/Data/AppDbContext.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BudgetApp.Domain.Entities; //Hämta in allt som finns i Enities mappen i Domain projektet. 
+using Microsoft.EntityFrameworkCore; // Hämtar in alla klasser DbContext och Dbset från EF Core biblioteket.
 
-namespace BudgetApp.Infrastructure.Data
+//DbContext hanterar all kommunikation med databasen. Och min AppDbContext ärver från DbContext klassen i EF Core.
+//Dbset är EF Copres klass som representerar en tabell i databasen. Varje Dbset i AppDbContext representerar en tabell i databasen som kommer att skapas baserat på mina entiteter (Budget, Category, Transactions).
+
+namespace Budget.App.Infrastructure.Data //talar om var i projektet den här klassen bor
 {
-    internal class AppDbContext
+    public class AppdbContext : DbContext //Min AppDbContext klass ärver från DbContext klassen i EF Core. För att den hanterar all kommunikation med databasen
     {
+        //Konstruktor - en metoden som körs när en instans av klassen skapas. Den tar emot DbContextOptions som innehåller konfigurationen för databaskopplingen och skickar den vidare till bas-klassen (DbContext) genom att använda base(options).
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //Varje DbSet representerar en tabell i databasen. 
+        //EF tittar på dina entiteter och skapar tabellerna automatiskt utifrån dem när du kör en migration.
+        //Migration betyder att det är EF Core sätt att översätta mina Csharp klasser till databastabeller
+        public Dbset<Transaction> Transactions { get; set; }
+        public Dbset<Budget> Budgets { get; set; }
+        public Dbset<Category> Categories { get; set; }
     }
 }
--- BudgetApp.Infrastructure/Repositories/DependencyInjection.cs
+++
@@ -1,2 +0,0 @@
-﻿using BudgetApp.Domain.Entities;
-using Microsoft.EntityFrameworkCore; //Jag hämtar nu alla mina entitieter från Domain och EF Core biblioteket.
