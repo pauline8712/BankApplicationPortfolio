bb893cc67d59e000b894ba01bcd2a5d6ba6e8ab2
Pauline <plinekaeorg@gmail.com>
2026-04-14 20:17:18 +0200

    Forgot to add some properties for my entities files

---
+++ BudgetApp.Domain/Entities/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp.Domain.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string? Category { get; set; }
+        public bool IsResolved { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
